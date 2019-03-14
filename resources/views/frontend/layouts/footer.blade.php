db2f9c77551d0c0715aa21f2ed2fb4e2336987c7
I Gede Pradipta Adi Nugraha <pradiptadipta31@gmail.com>
2019-03-14 15:33:25 +0800

    asd

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="footer-section">
   <div class="container">
     <div class="footer-top">
-      <p> &copy; 2019 Mepandes | Design by DNA Studio</p>
+      <p> &copy; 2019 Mepandes | Design by DNA Studio Indonesia</p>
     </div>
   </div>
 </div>
